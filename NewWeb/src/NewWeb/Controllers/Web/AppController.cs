1214143cfe853fefb8cb8ef96a4b31969b4c1493
Qlanowski <ulanowskikarol@gmail.com>
2016-09-01 10:22:14 +0200

    NewWeb
    With working authentication and DoctorId in User Table

---
+++ NewWeb/src/NewWeb/Controllers/Web/AppController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewWeb.Controllers.Web
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
